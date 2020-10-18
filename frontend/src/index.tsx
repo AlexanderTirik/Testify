4e97d4909662d7ce549f28a99d6f2e8ea92e2a66
AlexanderTirik <sasha.tirik@gmail.com>
2020-10-18 15:03:50 +0300

    add sass

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './scenes/App/App';
+import './styles/index.sass';
+import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
