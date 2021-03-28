385cedf0b80ac6a7554467daf5b066bfeba2fdf9
AlexanderTirik <sasha.tirik@gmail.com>
2021-03-28 23:23:20 +0300

    rename project

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const env = {
   app: {
-    port: getOsEnv('STORMRUST_PORT') || 3001,
+    port: getOsEnv('TESTIFY_PORT') || 3001,
     client: getOsEnv('REACT_APP_CLIENT'),
     server: getOsEnv('REACT_APP_SERVER'),
     secret: getOsEnv('JWT_SECRET_KEY')
