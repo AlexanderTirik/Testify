73703332559ec6ea616933b3bea0ccfeabc4c8e4
AlexanderTirik <sasha.tirik@gmail.com>
2021-04-18 22:27:49 +0300

    add fetching user if logged in

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Routes } from '../../common/enums/Routes';
@@ -6,14 +6,50 @@ import Dashboard from '../../scenes/Dashboard/containers/Dashboard';
 import PrivateRoute from '../PrivateRoute';
 import LoginPage from '../../scenes/Authorization/containers/LoginPage';
 import LoginProcess from '../../scenes/Authorization/containers/LoginProcess';
+import { getAccessToken } from '../../common/helpers/storageHelper';
+import { IAppState } from '../../common/models/store/IAppState';
+import { fetchUserRoutine } from '../../routines/user';
+import { connect } from 'react-redux';
+import { IBindingAction } from '../../common/models/callback/IBindingAction';
+import { LoaderWrapper } from '../../components/LoaderWrapper';
 
-const Routing: FunctionComponent = () => (
-  <Switch>
-    <Route path={Routes.LoginProcess} component={LoginProcess} />
-    <Route path={Routes.LoginPage} component={LoginPage} />
-    <PrivateRoute path={Routes.Dashboard} component={Dashboard} />
-    <Redirect to={Routes.Dashboard} />
-  </Switch>
-);
+interface IProps {
+  isAuthorized: boolean;
+  isLoading: boolean;
+  fetchUser: IBindingAction;
+}
 
-export default Routing;
+const Routing: FunctionComponent<IProps> = ({ isAuthorized, isLoading, fetchUser }) => {
+  const hasToken = Boolean(getAccessToken());
+
+  useEffect(() => {
+    if (hasToken && !isAuthorized && !isLoading) {
+      fetchUser();
+    }
+  });
+
+  return (
+    <LoaderWrapper loading={isLoading || (hasToken && !isAuthorized)}>
+      <Switch>
+        <Route path={Routes.LoginProcess} component={LoginProcess} />
+        <Route path={Routes.LoginPage} component={LoginPage} />
+        <PrivateRoute path={Routes.Dashboard} component={Dashboard} />
+        <Redirect to={Routes.Dashboard} />
+      </Switch>
+    </LoaderWrapper>
+  );
+};
+
+const mapStateToProps = ({ user }: IAppState) => ({
+  isAuthorized: user.isAuthorized,
+  isLoading: user.isLoading
+});
+
+const mapDispatchToProps = {
+  fetchUser: fetchUserRoutine
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Routing);
